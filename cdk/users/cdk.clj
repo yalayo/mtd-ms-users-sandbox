50e7e439a42dd1ad94d0eccf79ec49a397dcf567
Mylab <labs@onmycrowd.com>
2020-05-19 21:53:30 -0700

    Adding infrastructure as code for deployment

---
+++ cdk/users/cdk.clj
@@ -0,0 +1,53 @@
+(ns users.cdk
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [stedi.cdk.alpha :as cdk]
+            [uberdeps.api :as uberdeps]))
+
+(cdk/import [[App Construct Duration Stack] :from "@aws-cdk/core"]
+            [[Bucket] :from "@aws-cdk/aws-s3"]
+            [[Code Function Runtime Tracing] :from "@aws-cdk/aws-lambda"])
+
+(defn- clean
+  []
+  (let [f (io/file "classes")]
+    (when (.exists f)
+      (->> f
+           (file-seq)
+           (reverse)
+           (map io/delete-file)
+           (dorun)))))
+
+(def code
+  (let [jarpath "target/app.jar"
+        deps    (edn/read-string (slurp "deps.edn"))]
+    (with-out-str
+      (clean)
+      (io/make-parents "classes/.")
+      (io/make-parents jarpath)
+      (compile 'users.client)
+      (compile 'users.lambda)
+      (uberdeps/package deps jarpath {:aliases [:classes]}))
+    (Code/fromAsset jarpath)))
+
+(def app (App))
+
+(def stack (Stack app "generate-user-lambda"))
+
+(def bucket (Bucket stack "generate-user-lambda-bucket"))
+
+(def my-fn
+  (Function stack
+            "send-topup-fn"
+            {:code        code        ;; Calling a static method
+             :handler     "com.anudis.hmrc.sandbox.GenerateUser"
+             :runtime     (:JAVA_8 Runtime)               ;; Getting a static property
+             :environment {"BUCKET" (:bucketName bucket)} ;; Getting an instance property
+             :memorySize 512
+             :timeout (Duration/seconds 30)
+             }))
+
+;; We can grant the function write access to the bucket using an
+;; instance method
+
+(Bucket/grantWrite bucket my-fn)
